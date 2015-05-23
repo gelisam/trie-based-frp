1b6a658b3a5a3bb7f1a72bb3e0b7e1ea480b15f5
Samuel Gélineau <gelisam+github@gmail.com>
2015-05-23 14:02:17 -0400

    test never and union
    the union test demonstrates that my implementation of interpret is
    incorrect :(

--- src/TrieBasedFRP/Combinators.hs
+++ src/TrieBasedFRP/Combinators.hs
@@ -4,9 +4,18 @@ import Data.MemoTrie
 
 import TrieBasedFRP.Types
 
+-- $setup
+-- >>> import TrieBasedFRP.Interpret
 
+
+-- |
+-- >>> interpret (\_ -> never) [[1,2,3],[4,5,6]]
+-- [[],[]]
 never :: HasTrie t => EventT f t a
 never = mempty
 
+-- |
+-- >>> interpret (\e -> e `union` e) [[1,2,3],[4,5,6]]
+-- [[1,2,3,1,2,3],[4,5,6,4,5,6]]
 union :: HasTrie t => EventT f t a -> EventT f t a -> EventT f t a
 union = mappend
