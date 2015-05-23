5eea69a54752ec32c498b0a061085cd2e437b1ad
Samuel Gélineau <gelisam+github@gmail.com>
2015-05-23 14:38:07 -0400

    spill

--- src/TrieBasedFRP/Combinators.hs
+++ src/TrieBasedFRP/Combinators.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module TrieBasedFRP.Combinators where
 
 import Data.MemoTrie
 
 import TrieBasedFRP.Types
+import TrieBasedFRP.Weaken
 
 -- $setup
 -- >>> import TrieBasedFRP.Interpret
@@ -19,3 +21,17 @@ never = mempty
 -- [[1,2,3,1,2,3],[4,5,6,4,5,6]]
 union :: HasTrie t => EventT f t a -> EventT f t a -> EventT f t a
 union = mappend
+
+
+-- |
+-- >>> interpret spill [[[1,2,3]],[[4],[5,6]]]
+-- [[1,2,3],[4,5,6]]
+spill :: forall t a. HasTrie t
+      => Event t [a]
+      -> Event t a
+spill e = mkEvent go (spill (weaken e))
+  where
+    go :: t -> ([a], Event t a)
+    go t = (concat xss, spill e')
+      where
+        (xss, e') = runEvent e t
