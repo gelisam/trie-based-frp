da6e6f335bb1ed3d8bd2ab18c8db5d8d137ba3f9
Samuel Gélineau <gelisam+github@gmail.com>
2015-05-23 13:33:37 -0400

    doctest setup

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,14 @@
 module Main where
 
+import Test.DocTest
+
 import TrieBasedFRP
 
 
 -- TODO: demonstrate how to build and use a dynamic event network.
 
+-- |
+-- >>> 1+1
+-- 2
 main :: IO ()
-main = putStrLn "typechecks."
+main = doctest ["-isrc", "src/Main.hs"]
